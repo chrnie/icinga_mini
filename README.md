f7bf71fd7de928167fa2f165cae8598baa606740
Christoph Niemann <christoph.niemann@netways.de>
2016-11-14 20:37:30 +0100

    added parameters and icinga2 command

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # icinga_mini
 mini check plugins for icinga, containing dummy and random checks
+
+# return_time_based_state.sh
+This plugin returns fake states for general and report testing purposes
+
+``` 
+object CheckCommand "return_time_based_state" {
+	import "plugin-check-command"
+
+	command = [ PluginDir + "icinga_mini/return_time_based_state.sh" ]
+
+	arguments = {
+		"ok" = {
+			value = "$hour_OK$"
+			description = "return OK before this time (hour, int)"
+			order = 1
+			skip_key = true
+		}
+		"warn" = {
+			value = "$hour_WARN$"
+			description = "return WARN between OK and this time (hour, int)"
+			order = 2
+			skip_key = true
+		}
+	}
+}
+``` 
--- return_time_based_state.sh
+++ return_time_based_state.sh
@@ -1,23 +1,56 @@
 #!/bin/bash
 
+##                  ##
+#   some functions   #
+##                  ##
+
+usage() {
+echo "This plugin returns fake states for general and report testing purposes"
+echo "$0 <before_OK, INT, HOUR OF DAY> <before_WARN, INT, HOUR OF DAY>"
+}
+not_a_number() {
+  echo "error: Not a number" >&2;
+  usage
+  exit 3
+}
+check_params() {
+  case $1 in
+    ''|*[!0-9]*) not_a_number ;;
+  esac
+  case $2 in
+    ''|*[!0-9]*) not_a_number ;;
+  esac
+}
+
+##             ##
+#  check vars   #
+##             ##
+
+check_params $1 $2
+
+##             ##
+#  check time   #
+##             ##
+
 HOUR=`date +%H`
 
-if [ "$HOUR" -lt 12 ]
+if [ "$HOUR" -lt $1 ]
   then
   echo -n "OK - Service is in Time"
   EXIT=0
-elif [ "$HOUR" -lt 13 ]
+elif [ "$HOUR" -lt $2 ]
   then
   echo -n "Warning - Service is in Lunchtime"
   EXIT=1
 else 
   echo -n "Critical - Service is out of Time"
-  echo "no"
   EXIT=2
 fi
 
-
+##       ##
+#  exit   #
+##       ##
 # perfdata format:  'label'=value[UOM];[warn];[crit];[min];[max]
-echo " | 'hour'=$HOUR;12;13;0;24"
+echo " | 'hour'=$HOUR;$1;$2;0;24"
 
 exit $EXIT
